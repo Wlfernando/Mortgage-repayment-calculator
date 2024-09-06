5cc4eac58ea32c9c6ec0a5517163b7dca0227608
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-09-06 03:44:27 -0600

    Feat: implement components to the DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { useState } from 'react'
 import './App.css'
+import Calculator from './components/Calculator/Calculator'
+import type { Mortgage } from './utils/types';
+import Result from './components/Results/Results';
+
+const emptyMortgage = {monthly: '', total: ''};
 
 function App() {
-
+  const [reset, setReset] = useState(0);
+  const [mortgage, setMortgage] = useState<Mortgage>(emptyMortgage);
 
   return (
     <>
-      
+      <main className='main'>
+        <Calculator 
+          reset={() => (setReset(reset + 1), setMortgage(emptyMortgage))}
+          key={reset}
+          onSubmit={(mortgage: Mortgage) => setMortgage(mortgage)}
+        />
+        <Result 
+          monthly={mortgage.monthly}
+          total={mortgage.total}
+        />
+      </main>
     </>
   )
 }
